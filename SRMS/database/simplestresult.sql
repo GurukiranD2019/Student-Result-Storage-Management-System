e2fdbea5fbc467d3c83bcfc85d98b38d2a9b42c0
Gurukiran D <89570807+GurukiranD2019@users.noreply.github.com>
2021-09-29 20:17:09 +0530

    Add files via upload

---
+++ SRMS/database/simplestresult.sql
@@ -0,0 +1,117 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 29, 2021 at 04:21 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `simplestresult`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_login`
+--
+
+CREATE TABLE `admin_login` (
+  `userid` varchar(30) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+
+--
+-- Dumping data for table `admin_login`
+--
+
+INSERT INTO `admin_login` (`userid`, `password`) VALUES
+('Student', 'D00F5D5217896FB7FD601412CB890830');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `class`
+--
+
+CREATE TABLE `class` (
+  `name` varchar(30) NOT NULL,
+  `id` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `class`
+--
+
+INSERT INTO `class` (`name`, `id`) VALUES
+('One', 1),
+('Two', 2),
+('Three', 3),
+('Four', 4),
+('Five', 5),
+('Six', 6),
+('Seven', 7),
+('Eight', 8);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `result`
+--
+
+CREATE TABLE `result` (
+  `name` varchar(30) NOT NULL,
+  `rno` int(3) NOT NULL,
+  `class` varchar(30) NOT NULL,
+  `p1` int(3) NOT NULL,
+  `p2` int(3) NOT NULL,
+  `p3` int(3) NOT NULL,
+  `p4` int(3) NOT NULL,
+  `p5` int(3) NOT NULL,
+  `marks` int(3) NOT NULL,
+  `percentage` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `result`
+--
+
+INSERT INTO `result` (`name`, `rno`, `class`, `p1`, `p2`, `p3`, `p4`, `p5`, `marks`, `percentage`) VALUES
+('Apeksh S', 19, 'Two', 98, 89, 92, 83, 85, 447, 89.4),
+('Gurukiran', 471, 'One', 89, 98, 87, 84, 92, 450, 90);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE `students` (
+  `name` varchar(30) NOT NULL,
+  `rno` int(3) NOT NULL,
+  `class_name` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`name`, `rno`, `class_name`) VALUES
+('Gurukiran', 471, 'One'),
+('Apeksh S', 19, 'Two');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
